291d79faf4ad617b38222b4c1463ec6257a5701a
adan222 <adanonline2@gmail.com>
2020-08-04 00:04:09 +0200

    Add all photos
    Add all photos as background image

--- script/js.js
+++ script/js.js
@@ -2,8 +2,7 @@ let circle = document.querySelectorAll(".progress_ring__circle");
 const radius = circle[0].r.baseVal.value;
 let circumference = radius * 2 * Math.PI;
 
-console.log(circle);
-
+//fucking circle
 function setProgress(whith) {
     let percent = circle[whith].getAttribute("percent");
 
@@ -11,10 +10,63 @@ function setProgress(whith) {
     circle[whith].style.strokeDashoffset = offset;
 }
 
-for(let i = 0; i < 6; i++){
+//seting images size 
+function setImageSize(){
+    let sec = document.querySelectorAll(".sec");
+    let howMuch = 0;
 
-    circle[i].style.strokeDasharray = `${circumference}  ${circumference}`;
-    circle[i].style.strokeDashoffset = `${circumference}`;
+    if(window.innerWidth <= 500){
+        howMuch = 3;                
+    }
+    else{
+        howMuch = 7;
+    }
 
-    setProgress(i);
+    let whatSize = window.innerWidth / howMuch;
+
+    for(let i = 0; i < sec.length; i++){
+        let child = sec[i].children;
+        for(let j = 0; j < howMuch; j++){
+            child[j].style.width = whatSize + "px";
+        }
+    }
 }
+
+function setImage(){
+    let sec = document.querySelectorAll(".sec");
+
+    setImageSize();
+
+    for(let i = 0; i < sec.length; i++){
+        let child = sec[i].children;
+
+        for(let j = 0; j < child.length; j++){
+            //get source to 
+            let src = child[j].getAttribute('src');
+
+            //this is what will be in style
+            let img = "url('" + src + "')";
+
+            //set style
+            child[j].style.backgroundImage = img;
+        }
+    }
+}
+
+
+//main function :)
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.querySelector("body").style.width = window.Width + "px";
+
+    for(let i = 0; i < 6; i++){
+
+        circle[i].style.strokeDasharray = `${circumference}  ${circumference}`;
+        circle[i].style.strokeDashoffset = `${circumference}`;
+    
+        setProgress(i);
+    }
+
+    setImage();
+});
+
